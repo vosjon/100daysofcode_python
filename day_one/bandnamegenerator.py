50406b3be9145bac699e27b04c5bc37fefcff8dc
vosjon <127369070+vosjon@users.noreply.github.com>
2023-05-17 17:43:48 -0700

    Create bandnamegenerator.py
    Day 1 of 100 project.

---
+++ day_one/bandnamegenerator.py
@@ -0,0 +1,14 @@
+# This is the Band Name Generator program.
+# Day 1 of the 100 Days of Code Course
+
+print("Welcome to the Band Name Generator!")
+
+name = input("Yo dude, enter your name: ")
+
+city = input("What city did you grow up in? ")
+
+pet_name = input("What is your pet's name? ")
+
+band_name = city + " " + pet_name
+
+print(f"{name}, your gnarly band name is: {band_name}!")
